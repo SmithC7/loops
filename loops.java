43dd6c9d0e508303735c32bf158f2d121ed95a0b
apcs <apcs@mvhs-u528ap14.dcsdk12.org>
2013-09-03 08:07:04 -0600

    Equivalent For Loop

--- loops.java
+++ loops.java
@@ -1,11 +1,14 @@
 public class loops{
 	public static void main(String[] args) {
-		int x=1;
+		// int x=1;
 
-		while(x<=100){
-			System.out.println(x);
-			x++;
+		// while(x<=100){
+		// 	System.out.println(x);
+		// 	x++;
 
+		// }
+		for(int i=1; i<=100; i++){
+			System.out.println(i);
 		}
 	}
 
